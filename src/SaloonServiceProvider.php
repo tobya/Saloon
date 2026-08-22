2aff4c9d13d4445516b57340cdd6bdff568c3ee2
Toby Allen <toby@cookingisfun.ie>
2026-08-22 09:19:02 +0100

    tidy

--- src/SaloonServiceProvider.php
+++ src/SaloonServiceProvider.php
@@ -10,11 +10,7 @@ class SaloonServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
+     
         $package
             ->name('saloon')
          //   ->hasCommand(SaloonCommand::class)
