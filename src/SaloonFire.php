dbc84fab725ae8fb5418c9165acd24991cddc4f5
Toby Allen <toby@cookingisfun.ie>
2026-08-22 08:44:51 +0100

    add cache plugin

--- src/SaloonFire.php
+++ src/SaloonFire.php
@@ -5,15 +5,17 @@
 
   use Saloon\Http\Response;
   use Saloon\Http\Request;
+  use Saloon\CachePlugin\Traits\HasCaching;
 
   class SaloonFire
   {
         protected $disableCaching = false;
 
 
-        public function disableCaching($disableCaching = true)
+        public function disableCaching($disableCaching = true) : static
         {
             $this->disableCaching = $disableCaching;
+            return $this;
         }
 
 
@@ -26,7 +28,11 @@
         protected function applymodifiers(Request $request) : Request
         {
             if ($this->disableCaching){
+              if(method_exists($request,'disableCaching'))
+             // if(is_callable([$this, 'disableCaching']))
+              {
                 $request->disableCaching();
+              }
             }
             return $request;
         }
